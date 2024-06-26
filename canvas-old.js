cce056de2e880c02c0e9718db7d489b8f4ad093f
Urmimala Nath <thedatanoodler@gmail.com>
2024-06-26 22:00:16 +0530

    First Commit. Demo of Particle Motion when Canvas is Resized

--- canvas-old.js
+++ canvas-old.js
@@ -2,9 +2,7 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
 //c is context
-
 var c = canvas.getContext('2d');
 
 //Rectangles & Squares
@@ -72,27 +70,34 @@ var c = canvas.getContext('2d');
 // var dx = (Math.random() - 0.5)*20 ;
 // var dy = (Math.random() - 0.5)*20 ;
 // var radius = 50;
-var mouse = {
-  x:undefined,
-  y: undefined
-}
-var maxRadiusLimit = 40;
-
-window.addEventListener('mousemove', 
-  function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    console.log(mouse);
-})
-
-window.addEventListener('resize',
-  function(){
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
-)
 
 //Circle() is a Javascript Object
+
+//Mouse Object
+var mouse = {
+  x: undefined,
+  y: undefined
+}
+var maxRadius = 40;
+var minRadius = 2;
+
+
+
+window.addEventListener('mousemove', 
+  function(event){
+      mouse.x=event.x;
+      mouse.y=event.y;
+      console.log(mouse);
+});
+
+window.addEventListener('resize', 
+  function(event){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHealth;
+
+    init();
+});
+
 var random_color1 = 'rgb('
 + Math.floor(Math.random() * 256) + ',' 
 + Math.floor(Math.random() * 256) + ',' 
@@ -103,18 +108,7 @@ var random_color2 = 'rgb('
 + Math.floor(Math.random() * 256) + ',' 
 + Math.floor(Math.random() * 256) + ')';
 
-//to change the Color of the circles
-var colorArray = [
-    '#c5c7b6',
-    '#99b24f',
-    '#6d992a',
-    '#7ea4a9',
-    '#51553d',
-    '#FF1B1C',
-    '#FF7F11',
-    '#ACBFA4'
-];
-
+//This function spawns a Circle element with x,y cordinates having dx,dy velocity vector
 function Circle(x, y, dx, dy, radius){
   this.x = x;
   this.y = y;
@@ -122,73 +116,51 @@ function Circle(x, y, dx, dy, radius){
   this.dy = dy;
   this.radius = radius;
 
-  this.minRadiusLimit = radius;
-
-  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
-
   this.draw = function() {
-        c.beginPath();        
+        c.beginPath();
         c.strokeStyle = random_color1;
-        c.fillStyle = this.color;
+        c.fillStyle = random_color2;        
         c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
         c.stroke();
         c.fill();
   }
 
-  this.update = function(){
-    
-      //if statement ensures the x,y coords stay within screen
-      if (this.x + this.radius > innerWidth || this.x- this.radius <0){
-        this.dx = -this.dx;
-      }
-      if (this.y + this.radius > innerHeight || this.y - this.radius <0){
-        this.dy = -this.dy;
-      }
-    //This increments the x coordinate everytime the page refreshes
-      this.x += this.dx;
-      this.y += this.dy;
-
-      //interactivity
-      if (mouse.x - this.x < 50 && mouse.x - this.x  > -50){
-            if(this.radius < maxRadiusLimit ) {
-                  this.radius += 1;
-            }
-      }else if (this.radius > this.minRadiusLimit ){
-            this.radius -= 1;
-          }
-      if (mouse.y - this.y < 50 && mouse.y - this.y  > -50){
-        if(this.radius < maxRadiusLimit ) {
-            this.radius += 1;
-        }
-      } else if (this.radius > this.minRadiusLimit ){
-              this.radius -= 1;
-          }
-      
+  this.update = function(){    
+     
       this.draw();
       }
 }
-var circleArray = [];
 
-for (var i = 0 ; i < 300 ; i++){
+/* var circleArray = []; */
+
+/* for (var i = 0 ; i < 500 ; i++){
       var radius = Math.random() * 5 + 1;
       var x = Math.random() * (innerWidth - 2 * radius) + radius;
       var y = Math.random() * (innerHeight - 2 * radius) + radius;
       var dx = (Math.random() - 0.5) * 5 ;
-      var dy = (Math.random() - 0.5)  * 5 ;
+      var dy = (Math.random() - 0.5) * 5 ;
       circleArray.push(new Circle(x, y, dx, dy, radius));
       
-}
+} */
 console.log(circleArray);
 
 function animate(){
 
   requestAnimationFrame(animate);
   c.clearRect(0,0, innerWidth, innerHeight); //refresh screen
+  c.beginPath();
+        c.strokeStyle = random_color1;
+        c.fillStyle = random_color2;        
+        c.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        c.stroke();
+        c.fill();
 
-  for (var i = 0 ; i < circleArray.length ; i++){
+  /* for (var i = 0 ; i < circleArray.length ; i++){
       circleArray[i].update();
-  }
+  } */
 }
 animate();
 
-console.log(canvas);
+console.log('Screen width is: ',innerWidth);
+console.log('Screen height is: ',innerHeight);
+console.log('Canvas : ',canvas);
